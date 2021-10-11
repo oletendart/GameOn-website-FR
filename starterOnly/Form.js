1264c08c4eeda46d768792b11031e6bf622c4f0c
Letendart Olivia <olivia.letendart@socrate-edu.com>
2021-10-11 17:37:29 +0200

    ISSUE #3 finished

--- starterOnly/Form.js
+++ starterOnly/Form.js
@@ -8,10 +8,9 @@ class Form {
         this.email = document.getElementById('email').value;
         this.birthdate = false;
         this.tournament = document.getElementById('quantity');
-        this.chexboxObligatoire = document.getElementById('checkbox1');
+        this.chexboxRequired = document.getElementById('checkbox1');
         this.city = false;
         this.errors = 0;
-
     }
 
     validate() {
@@ -24,14 +23,26 @@ class Form {
         this.validateChexbox();
 
         if (this.errors === 0) {
-            // valider formulaire
-            // vidage du formulaire
-            this.clearCache();
             // cache la modale
+            /*closureModal();
+            document.getElementById("myform").reset();
             // affichage du message d'inscription ok
+            this.registrationMessage = prompt();*/
+
+
         }
 
     }
+
+    error(id, isError, msg) {
+        let parent = document.getElementById(id).parentNode;
+        parent.setAttribute('data-error-visible', isError);
+        parent.setAttribute('data-error', msg);
+    }
+
+    // registerMessage() {
+    //
+    // }
 
 
     validateFirstname() {
@@ -39,25 +50,16 @@ class Form {
         const firstnameRegex = /((^[A-Za-zÀÁÂÃÄÇÈÉÊËÌÍÎÏÒÓÔÕÖàáâäçéèêëîïõôö]+)([\-]{1}[A-Za-zÀÁÂÃÄÇÈÉÊËÌÍÎÏÒÓÔÕÖàáâäçéèêëîïõôö]+){0,1})$/gm;
 
         if (this.firstname.length < 2 || this.firstname.length > 20) {
-            console.log('erreur first longueur');
-            this.errors += 1;
-            // message d'erreur en dessous du bon champ
-            // ajoutera 1 à une valeur défini dans le constructeur
-            /*
-            errorDisplay("first", "Le prénom doit faire entre 2 et 20 caractères");
-            */
-        } else if (!firstnameRegex.test(this.firstname)) {
-            console.log('erreur first regex');
-            this.errors += 1;
-            // message d'erreur en dessous du bon champ
-            // ajoutera 1 à une valeur défini dans le constructeur
-            /*
-            errorDisplay("first", "Le prénom doit faire entre 2 et 20 caractères");
-            */
-        } else {
-            console.log('valide first');
-            /*errorDisplay("first", "", true);*/
+            this.errors++;
 
+            this.error("first", true, "Veuillez entrer 2 caractères ou plus.");
+
+        } else if (!firstnameRegex.test(this.firstname)) {
+            this.errors++;
+
+            this.error('first', true, "Veuillez saisir un prénom valide.")
+        } else {
+            this.error('first', false);
         }
     }
 
@@ -66,24 +68,15 @@ class Form {
         const lastnameRegex = /((^[A-Za-zÀÁÂÃÄÇÈÉÊËÌÍÎÏÒÓÔÕÖàáâäçéèêëîïõôö]+)([\s]{1}[A-Za-zÀÁÂÃÄÇÈÉÊËÌÍÎÏÒÓÔÕÖàáâäçéèêëîïõôö]+){0,1})$/gm;
 
         if (this.lastname.length < 2 || this.lastname.length > 20) {
-            console.log('erreur last longueur');
-            this.errors += 1;
-            // message d'erreur en dessous du bon champ
-            // ajoutera 1 à une valeur défini dans le constructeur
-            /*
-            errorDisplay("first", "Le prénom doit faire entre 2 et 20 caractères");
-            */
+            this.errors++;
+
+            this.error('last', "Veuillez entrer 2 caractères ou plus.", true);
         } else if (!lastnameRegex.test(this.lastname)) {
-            console.log('erreur last regex');
-            this.errors += 1;
-            // message d'erreur en dessous du bon champ
-            // ajoutera 1 à une valeur défini dans le constructeur
-            /*
-            errorDisplay("first", "Le prénom doit faire entre 2 et 20 caractères");
-            */
+            this.errors++;
+
+            this.error('last', "Veuillez saisir un nom valide.", true);
         } else {
-            console.log('valide last');
-            /*errorDisplay("first", "", true);*/
+            this.error('last', '', false);
 
         }
     }
@@ -91,107 +84,113 @@ class Form {
 
     validateEmail() {
 
-        if (!this.email.match(/(^[a-z0-9]+[\-\.]*[a-z0-9]+[\@][a-z]+[\-\.]*[a-z0-9]+[\.]{1}[a-z]{2,})$/gm)) {
-            console.log('erreur email regex');
-            this.errors += 1;
+        if (this.email.length < 2 || this.email.length > 20 ) {
+            this.errors++;
+
+            this.error('email', "Veuillez remplir le champ.", true);
+        } else if (!this.email.match(/(^[a-z0-9]+[\-\.]*[a-z0-9]+[\@][a-z]+[\-\.]*[a-z0-9]+[\.]{1}[a-z]{2,})$/gm)) {
+            this.errors++;
+
+            this.error('email', "Veuillez saisir une adresse mail valide.", true);
         } else {
-            console.log('valid email');
+            this.error('email', "", false);
         }
 
     }
 
+    refreshDate(e) {
+        e.setHours(0);
+        e.setMinutes(0)
+        e.setSeconds(0);
+        e.setMilliseconds(0);
+    }
+
+    refreshDateBirthday(e, d) {
+        e.setFullYear(d[0]);
+        e.setMonth(d[1] - 1);
+        e.setDate(d[2]);
+        e.setHours(0);
+        e.setMinutes(0);
+        e.setSeconds(0);
+        e.setMilliseconds(0);
+    }
+
     validateBirthday() {
         let sixteenDate = new Date();
-        sixteenDate.setHours(0);
-        sixteenDate.setMinutes(0);
-        sixteenDate.setSeconds(0);
-        sixteenDate.setMilliseconds(0);
-        console.log(this.birthdate.value);
+        this.refreshDate(sixteenDate);
         // split
         let birthdayInput = document.getElementById('birthdate');
         if (birthdayInput.value) {
             let dateOfBirthday = new Date();
             let date = birthdayInput.value.split('-');
-            console.log(date);
-            dateOfBirthday.setFullYear(date[0]);
-            dateOfBirthday.setMonth(date[1] - 1);
-            dateOfBirthday.setDate(date[2]);
-            dateOfBirthday.setHours(0);
-            dateOfBirthday.setMinutes(0);
-            dateOfBirthday.setSeconds(0);
-            dateOfBirthday.setMilliseconds(0);
+            this.refreshDateBirthday(dateOfBirthday, date);
             this.birthdate = dateOfBirthday;
         }
-        console.log("birthday" + this.birthdate);
-
 
         sixteenDate.setFullYear(sixteenDate.getFullYear() - 16);
-        console.log("sixteendate" + sixteenDate);
-
 
         if (!this.birthdate) {
-            console.log('Remplir champ anniversaire');
-            this.errors += 1;
+            this.errors++;
+
+            this.error('birthdate', "Veuillez remplir le champ.", true);
         } else if (sixteenDate < this.birthdate) {
-            console.log('erreur birtdhay');
-            this.errors += 1;
+            this.errors++;
+
+            this.error('birthdate', "Vous devez avoir plus de 16 ans.", true);
         } else {
-            console.log('valide birthday')
+            this.error('birthdate', '', false);
         }
     }
 
     validateTournament() {
         if (!this.tournament.value) {
-            console.log('Veuillez remplir le champ tournament');
-            this.errors += 1;
+            this.errors++;
+
+            this.error('quantity', "Veuillez remplir le champ.", true);
         } else if (isNaN(Number(this.tournament.value))) {
-            console.log('pas un chiffre');
-            this.errors += 1;
+            this.errors++;
+
+            this.error('quantity', "Veuillez saisir un chiffre.", true);
         } else {
-            console.log('valide tournament')
+            this.error('quantity', "", false);
         }
     }
 
     validateCity() {
         const radios = document.getElementsByName('location');
-        /*
-        const isValid = Array.from(radios).some((radio) => {
-            return radio.checked
-        });
-
-         */
-
 
         for (let i = 0; i < radios.length; i++) {
             if (radios[i].type === 'radio' && radios[i].checked) {
                 this.city = radios[i].value;
-                console.log('okai city');
+
+                this.error('location1', "", false);
             }
         }
 
-
         if (!this.city) {
-            console.log('veuillez choisir une city');
             this.errors++;
+
+            this.error('location1', "Veuillez choisir une ville", true);
         }
     }
 
     validateChexbox() {
-        if (!this.chexboxObligatoire.checked) {
-            console.log('erreur chexbox');
-            this.errors += 1;
+        if (!this.chexboxRequired.checked) {
+            this.errors++;
+
+            this.error('checkbox1', "Vous devez vérifier que vous acceptez les termes et conditions.", true);
         } else {
-            console.log('valide checkbox')
+            this.error('checkbox1', "", false);
         }
     }
 
-    clearCache() {
+    /*clearCache() {
         this.firstname = "";
         this.lastname = "";
         this.email = "";
         this.birthdate.value = "";
         this.tournament.value = "";
-    }
+    }*/
 
 }
 
